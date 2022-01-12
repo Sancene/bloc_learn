cbed9693efad5e640f899e7ce416b158916bd4fb
George <George.Lehr@gmail.com>
2022-01-12 11:14:18 +0300

    initial commit

---
+++ android/app/src/main/kotlin/com/bloc_learn/bloc_learn/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.bloc_learn.bloc_learn
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:authentication_repository/authentication_repository.dart';
+import 'package:bloc/bloc.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/widgets.dart';
+import 'package:bloc_learn/app/app.dart';
+
+import 'app/bloc_observer.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  final authenticationRepository = AuthenticationRepository();
+  await authenticationRepository.user.first;
+  BlocOverrides.runZoned(
+        () => runApp(App(authenticationRepository: authenticationRepository)),
+    blocObserver: AppBlocObserver(),
+  );
+}
