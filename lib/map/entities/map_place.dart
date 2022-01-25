33ad9903dc314877101923dbf405b4a6634aa34a
George <George.Lehr@gmail.com>
2022-01-25 15:43:26 +0300

    excess model fields deleted

--- lib/map/entities/map_place.dart
+++ lib/map/entities/map_place.dart
@@ -7,9 +7,8 @@ class MapPlace {
   final String openingHours;
   final String name;
   final String address;
-  final int id;
 
-  MapPlace(this.coordinates, this.openingHours, this.name, this.address, this.id);
+  MapPlace(this.coordinates, this.openingHours, this.name, this.address);
 }
 
 double _randomDouble() {
@@ -26,7 +25,6 @@ final List<MapPlace> testData = [
     "8:00-17:45",
     "Boston cafee",
     "Улица Пушкина, Дом Колотушкина",
-    1,
   ),
   MapPlace(
     Point(
@@ -36,7 +34,6 @@ final List<MapPlace> testData = [
     "9:00-12:45",
     "Boston arolfer",
     "Улица Пушкина, Дом Колотушкина",
-    2,
   ),
   MapPlace(
     Point(
@@ -46,7 +43,6 @@ final List<MapPlace> testData = [
     "8:15-17:35",
     "American cafee",
     "Улица Пушкина, Дом Колотушкина",
-    3,
   ),
   MapPlace(
     Point(
@@ -56,7 +52,6 @@ final List<MapPlace> testData = [
     "8:00-17:45",
     "Mega cafee",
     "Улица Пушкина, Дом Колотушкина",
-    4,
   ),
   MapPlace(
     Point(
@@ -66,6 +61,5 @@ final List<MapPlace> testData = [
     "8:00-17:45",
     "Boston cafee",
     "Улица Пушкина, Дом Колотушкина",
-    5,
   ),
 ];
