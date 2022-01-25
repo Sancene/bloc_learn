f55c5696615a4c1d9e4aab1ccc1a71b0db3f5092
George <George.Lehr@gmail.com>
2022-01-25 13:48:18 +0300

    cluster tap zoom in + focus

--- lib/map/widget.dart
+++ lib/map/widget.dart
@@ -12,11 +12,8 @@ abstract class MapPage extends StatelessWidget {
 }
 
 class ControlButton extends StatelessWidget {
-  const ControlButton({
-    Key? key,
-    required this.onPressed,
-    required this.title
-  }) : super(key: key);
+  const ControlButton({Key? key, required this.onPressed, required this.title})
+      : super(key: key);
 
   final VoidCallback onPressed;
   final String title;
@@ -51,18 +48,18 @@ class _ClusterizedPlacemarkCollectionExample extends StatefulWidget {
 
 class _ClusterizedPlacemarkCollectionExampleState
     extends State<_ClusterizedPlacemarkCollectionExample> {
-
   late YandexMapController controller;
-  final animation = const MapAnimation(type: MapAnimationType.smooth, duration: 1.0);
+  final animation =
+      const MapAnimation(type: MapAnimationType.smooth, duration: 1.0);
 
   final List<MapObject> mapObjects = [];
 
   final int kPlacemarkCount = 500;
   final Random seed = Random();
-  final MapObjectId clusterizedPlacemarkCollectionId = MapObjectId(
-      'clusterized_placemark_collection');
-  final MapObjectId largeClusterizedPlacemarkCollectionId = MapObjectId(
-      'large_clusterized_placemark_collection');
+  final MapObjectId clusterizedPlacemarkCollectionId =
+      MapObjectId('clusterized_placemark_collection');
+  final MapObjectId largeClusterizedPlacemarkCollectionId =
+      MapObjectId('large_clusterized_placemark_collection');
 
   Future<Uint8List> _buildClusterAppearance(Cluster cluster) async {
     final recorder = PictureRecorder();
@@ -80,10 +77,8 @@ class _ClusterizedPlacemarkCollectionExampleState
     final textPainter = TextPainter(
         text: TextSpan(
             text: cluster.size.toString(),
-            style: TextStyle(color: Colors.black, fontSize: 50)
-        ),
-        textDirection: TextDirection.ltr
-    );
+            style: TextStyle(color: Colors.black, fontSize: 50)),
+        textDirection: TextDirection.ltr);
 
     textPainter.layout(minWidth: 0, maxWidth: size.width);
 
@@ -95,8 +90,9 @@ class _ClusterizedPlacemarkCollectionExampleState
     canvas.drawCircle(circleOffset, radius, strokePaint);
     textPainter.paint(canvas, textOffset);
 
-    final image = await recorder.endRecording().toImage(
-        size.width.toInt(), size.height.toInt());
+    final image = await recorder
+        .endRecording()
+        .toImage(size.width.toInt(), size.height.toInt());
     final pngBytes = await image.toByteData(format: ImageByteFormat.png);
 
     return pngBytes!.buffer.asUint8List();
@@ -106,145 +102,165 @@ class _ClusterizedPlacemarkCollectionExampleState
     return (500 - seed.nextInt(1000)) / 1000;
   }
 
+  Point getClusterCoordinates(Cluster cluster) {
+    int size = cluster.placemarks.length;
+    double latitude = 0;
+    double longitude = 0;
+    cluster.placemarks.forEach(
+      (placemark) {
+        latitude += placemark.point.latitude;
+        longitude += placemark.point.longitude;
+      },
+    );
+    return Point(latitude: latitude / size, longitude: longitude / size);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: <Widget>[
-          Expanded(
-              child: YandexMap(
-                  mapObjects: mapObjects,
-                  logoAlignment: MapAlignment(horizontal: HorizontalAlignment.left, vertical: VerticalAlignment.bottom),
-                  onMapCreated: (
-                      YandexMapController yandexMapController) async {
-                    controller = yandexMapController;
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
+        Expanded(
+            child: YandexMap(
+          mapObjects: mapObjects,
+          logoAlignment: MapAlignment(
+              horizontal: HorizontalAlignment.left,
+              vertical: VerticalAlignment.bottom),
+          onMapCreated: (YandexMapController yandexMapController) async {
+            controller = yandexMapController;
 
-                    controller.moveCamera(CameraUpdate.zoomTo(8));
-                    controller.moveCamera(CameraUpdate.newCameraPosition(CameraPosition(target: Point(latitude: 55.756, longitude: 37.618))));
-                    final cameraPosition = await controller.getCameraPosition();
-                    final minZoom = await controller.getMinZoom();
-                    final maxZoom = await controller.getMaxZoom();
+            controller.addListener(() {});
+            controller.moveCamera(CameraUpdate.zoomTo(8));
+            controller.moveCamera(CameraUpdate.newCameraPosition(CameraPosition(
+                target: Point(latitude: 55.756, longitude: 37.618))));
+            final cameraPosition = await controller.getCameraPosition();
+            final minZoom = await controller.getMinZoom();
+            final maxZoom = await controller.getMaxZoom();
 
-                    print('Camera position: $cameraPosition');
-                    print('Min zoom: $minZoom, Max zoom: $maxZoom');
-                  },
-              )
-          ),
-          const SizedBox(height: 20),
-          Container(
-            child: SingleChildScrollView(
-                child: Column(
-                    children: <Widget>[
-                      Text('Set of $kPlacemarkCount placemarks'),
-                      Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceAround,
-                          children: <Widget>[
-                            ControlButton(
-                                onPressed: () async {
-                                  if (mapObjects.any((el) =>
-                                  el.mapId ==
-                                      largeClusterizedPlacemarkCollectionId)) {
-                                    return;
-                                  }
-                                  final clusterizedPlacemarkCollection = ClusterizedPlacemarkCollection(
-                                    mapId: largeClusterizedPlacemarkCollectionId,
-                                    radius: 30,
-                                    minZoom: 15,
-                                    onClusterAdded: (
-                                        ClusterizedPlacemarkCollection self,
-                                        Cluster cluster) async {
-                                      return cluster.copyWith(
-                                          appearance: cluster.appearance
-                                              .copyWith(
-                                              opacity: 0.75,
-                                              icon: PlacemarkIcon.single(
-                                                  PlacemarkIconStyle(
-                                                      image: BitmapDescriptor
-                                                          .fromBytes(
-                                                          await _buildClusterAppearance(
-                                                              cluster)),
-                                                      scale: 1
-                                                  ))
-                                          )
-                                      );
-                                    },
-                                    onClusterTap: (
-                                        ClusterizedPlacemarkCollection self,
-                                        Cluster cluster) {
-                                      //controller.moveCamera(CameraUpdate(TapDownDetails.globalPosition)),
-                                      controller.moveCamera(CameraUpdate.zoomIn(), animation: animation);
-                                      print('Tapped cluster');
-                                    },
-                                    placemarks: List<Placemark>.generate(
-                                        kPlacemarkCount, (i) {
-                                      return Placemark(
-                                        onTap: (placemark, point) {
-                                          showModalBottomSheet<void>(
-                                            context: context,
-                                            builder: (BuildContext context) {
-                                              return Container(
-                                                height: 200,
-                                                color: Colors.amber,
-                                                child: Center(
-                                                  child: Column(
-                                                    mainAxisAlignment: MainAxisAlignment.center,
-                                                    mainAxisSize: MainAxisSize.min,
-                                                    children: <Widget>[
-                                                      const Text('Modal BottomSheet'),
-                                                      ElevatedButton(
-                                                        child: const Text('Close BottomSheet'),
-                                                        onPressed: () => Navigator.pop(context),
-                                                      )
-                                                    ],
-                                                  ),
+            print('Camera position: $cameraPosition');
+            print('Min zoom: $minZoom, Max zoom: $maxZoom');
+          },
+        )),
+        const SizedBox(height: 20),
+        Container(
+          child: SingleChildScrollView(
+            child: Column(
+              children: <Widget>[
+                Text('Set of $kPlacemarkCount placemarks'),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: <Widget>[
+                    ControlButton(
+                        onPressed: () async {
+                          if (mapObjects.any((el) =>
+                              el.mapId ==
+                              largeClusterizedPlacemarkCollectionId)) {
+                            return;
+                          }
+                          final clusterizedPlacemarkCollection =
+                              ClusterizedPlacemarkCollection(
+                            mapId: largeClusterizedPlacemarkCollectionId,
+                            radius: 45,
+                            minZoom: 15,
+                            onClusterAdded:
+                                (ClusterizedPlacemarkCollection self,
+                                    Cluster cluster) async {
+                              return cluster.copyWith(
+                                  appearance: cluster.appearance.copyWith(
+                                      opacity: 0.75,
+                                      icon: PlacemarkIcon.single(
+                                          PlacemarkIconStyle(
+                                              image: BitmapDescriptor.fromBytes(
+                                                await _buildClusterAppearance(
+                                                  cluster,
                                                 ),
-                                              );
-                                            },
-                                          );
-                                        },
-                                          mapId: MapObjectId('placemark_$i'),
-                                          point: Point(latitude: 55.756 +
-                                              _randomDouble(),
-                                              longitude: 37.618 +
-                                                  _randomDouble()),
-                                          icon: PlacemarkIcon.single(
-                                              PlacemarkIconStyle(
-                                                  image: BitmapDescriptor
-                                                      .fromAssetImage(
-                                                      'assets/place.png'),
-                                                  scale: 1
-                                              ))
+                                              ),
+                                              scale: 1))));
+                            },
+                            onClusterTap: (ClusterizedPlacemarkCollection self,
+                                Cluster cluster) async {
+                              await controller.moveCamera(
+                                CameraUpdate.newCameraPosition(
+                                  CameraPosition(
+                                    target: getClusterCoordinates(cluster),
+                                    zoom: await controller
+                                        .getCameraPosition()
+                                        .then((value) => value.zoom + 1),
+                                  ),
+                                ),
+                                animation: animation,
+                              );
+                            },
+                            placemarks:
+                                List<Placemark>.generate(kPlacemarkCount, (i) {
+                              return Placemark(
+                                onTap: (placemark, point) {
+                                  showModalBottomSheet<void>(
+                                    context: context,
+                                    builder: (BuildContext context) {
+                                      return Container(
+                                        height: 200,
+                                        color: Colors.amber,
+                                        child: Center(
+                                          child: Column(
+                                            mainAxisAlignment:
+                                                MainAxisAlignment.center,
+                                            mainAxisSize: MainAxisSize.min,
+                                            children: <Widget>[
+                                              const Text('Modal BottomSheet'),
+                                              ElevatedButton(
+                                                child: const Text(
+                                                    'Close BottomSheet'),
+                                                onPressed: () =>
+                                                    Navigator.pop(context),
+                                              )
+                                            ],
+                                          ),
+                                        ),
                                       );
-                                    }),
-                                    onTap: (ClusterizedPlacemarkCollection self,
-                                        Point point) =>
-                                        print('Tapped me at $point'),
+                                    },
                                   );
+                                },
+                                mapId: MapObjectId('placemark_$i'),
+                                point: Point(
+                                    latitude: 55.756 + _randomDouble(),
+                                    longitude: 37.618 + _randomDouble()),
+                                icon: PlacemarkIcon.single(
+                                  PlacemarkIconStyle(
+                                      image: BitmapDescriptor.fromAssetImage(
+                                          'assets/place.png'),
+                                      scale: 1),
+                                ),
+                              );
+                            }),
+                            onTap: (ClusterizedPlacemarkCollection self,
+                                    Point point) =>
+                                print('Tapped Cluster?? me at $point'),
+                          );
 
-                                  setState(() {
-                                    mapObjects.add(
-                                        clusterizedPlacemarkCollection);
-                                  });
-                                },
-                                title: 'Add'
-                            ),
-                            ControlButton(
-                                onPressed: () async {
-                                  setState(() {
-                                    mapObjects.removeWhere((el) =>
-                                    el.mapId ==
-                                        largeClusterizedPlacemarkCollectionId);
-                                  });
-                                },
-                                title: 'Remove'
-                            )
-                          ]
-                      )
-                    ]
+                          setState(() {
+                            mapObjects.add(clusterizedPlacemarkCollection);
+                          });
+                        },
+                        title: 'Add'),
+                    ControlButton(
+                        onPressed: () async {
+                          setState(
+                            () {
+                              mapObjects.removeWhere((el) =>
+                                  el.mapId ==
+                                  largeClusterizedPlacemarkCollectionId);
+                            },
+                          );
+                        },
+                        title: 'Remove')
+                  ],
                 )
+              ],
             ),
-          )
-        ]
+          ),
+        )
+      ],
     );
   }
 }
